414c16afe0876fbf93cf76c8cdc38b86d3f2faa5
Adam <adamsouames08@gmail.com>
2025-10-20 16:44:40 +0200

    ajout entité Department

---
+++ src/main/java/com/unilim/erp/entities/Department.java
@@ -0,0 +1,63 @@
+package com.unilim.erp.entities;
+
+import java.util.UUID;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String code;
+
+    @Column(nullable = false)
+    private String label;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Department parent;
+
+    public Department() {
+    }
+
+    public Department(String code, String label, Department parent) {
+        this.code = code;
+        this.label = label;
+        this.parent = parent;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Department getParent() {
+        return parent;
+    }
+
+    public void setParent(Department parent) {
+        this.parent = parent;
+    }
+
+}
